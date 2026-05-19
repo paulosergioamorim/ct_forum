7633f90b3453a70dda4af26c0bcb5cbe78a6a5e1
Paulo Sérgio <pamorimmonico@gmail.com>
2026-05-19 20:21:46 -0300

    first commit

---
+++ src/main/java/com/ufes/ct_forum/CtForumApplication.java
@@ -0,0 +1,13 @@
+package com.ufes.ct_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufes/ct_forum/CtForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufes.ct_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
